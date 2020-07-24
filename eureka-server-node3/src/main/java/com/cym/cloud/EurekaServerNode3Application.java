0d4466b172d931bf767663e4e5b7f12c3e78757f
CYM <2863416607@qq.com>
2020-07-24 21:53:48 +0800

    cloud提交

---
+++ eureka-server-node3/src/main/java/com/cym/cloud/EurekaServerNode3Application.java
@@ -0,0 +1,15 @@
+package com.cym.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerNode3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerNode3Application.class, args);
+	}
+
+}
